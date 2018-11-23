dac61133aeae72fe2d471c815dbef185c0b75dff
Roldão Rego Jr <roldaogjr@gmail.com>
2018-11-23 20:14:31 +0000

    Adicionado campo situação e campo observações

--- trabalhos/models.py
+++ trabalhos/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
+from djchoices import DjangoChoices, ChoiceItem
 from eventos.models import Usuario, Inscricao
 
 
@@ -31,6 +32,12 @@ class Modalidade(models.Model):
 
 @python_2_unicode_compatible
 class Trabalho(models.Model):
+    class Situacoes(DjangoChoices):
+        Pendente = ChoiceItem(0)
+        Aprovado = ChoiceItem(1)
+        Corrigir = ChoiceItem(2, 'Aprovado com correções')
+        Reprovado = ChoiceItem(3)
+
     modalidade = models.ForeignKey(Modalidade, on_delete=models.CASCADE,
                                    related_name='trabalhos')
     area = models.ForeignKey(AreaTema, on_delete=models.CASCADE,
@@ -40,6 +47,11 @@ class Trabalho(models.Model):
                                   related_name='trabalhos')
     titulo = models.CharField(max_length=300)
     arquivo = models.FileField(upload_to='trabalhos', blank=False)
+    situacao = models.IntegerField(choices=Situacoes.choices,
+                                   default=Situacoes.Pendente,
+                                   verbose_name='situação')
+    observacao = models.TextField(blank=True, null=True,
+                                  verbose_name='observações')
 
     def __str__(self):
         return self.titulo
